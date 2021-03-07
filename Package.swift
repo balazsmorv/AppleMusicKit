91ffc0dcd9008e82544a255b416a10202b9586dc
Morvay Balázs <balazsmorvay@yahoo.com>
2021-03-07 19:09:58 +0100

    build fix

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppleMusicKit",
-            dependencies: ["RxSwift"]),
+            dependencies: ["RxSwift", "SwiftyJSON"]),
         .testTarget(
             name: "AppleMusicKitTests",
             dependencies: ["AppleMusicKit", "RxSwift", "SwiftyJSON"]),
